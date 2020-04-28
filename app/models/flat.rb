02bd13021a7623357f74b4e0d46f29eae2b1b5b1
Sy Rashid <sy.rashid.11@gmail.com>
2020-04-28 21:24:55 +1000

    ARCH: Added validations to flats model

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,6 @@
 class Flat < ApplicationRecord
+    validates :name, :address, :description, :price_per_night, :number_of_guests, presence: true
+    validates :number_of_guests, inclusion: { in: 0..24 }
+    validates :price_per_night, :number_of_guests, numericality: { only_integer: true }
+    validates :price_per_night, numericality: {greater_than: 50}
 end
